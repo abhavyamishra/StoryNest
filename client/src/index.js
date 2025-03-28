5bfe032aff8e76c25dcb2f47256184ed7b5bf82c
abhavyamishra <166243240+abhavyamishra@users.noreply.github.com>
2025-03-28 21:43:49 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,17 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
         <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   
   </React.StrictMode>
 
